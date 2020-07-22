b6fa4ad8a7a5ebf971c1dc70da1fd78451eb0b54
Neely Mann <NMann@talentpath.com>
2020-07-22 15:55:05 -0400

    UI grid

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import CWBA from './CustomersWhoBoughtAlsoAll'
 import QAcomponent from './QAcomponent';
+import ComparisonGrid from './ComparisonGrid';
 
 class App extends Component {
     render(){
@@ -9,6 +10,7 @@ class App extends Component {
                 <h1>My React App</h1>
                 <CWBA/>
                 <QAcomponent />
+                <ComparisonGrid/>
             </div>
         )
     }
