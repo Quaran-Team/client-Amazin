0f4d11c3401f0c24b2d424536805fac12239bbd8
JC <avalojc@gmail.com>
2020-07-22 10:21:03 -0400

    added CWBA cmpnt to AppJS and created CWBA cmpt

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import CWBA from './CustomersWhoBoughtAlso'
 
 class App extends Component {
     render(){
         return(
             <div>
                 <h1>My React App</h1>
+                <CWBA/>
             </div>
         )
     }
---
+++ src/components/CustomersWhoBoughtAlso.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+
+class CWBA extends Component {
+    render(){
+        return(
+            <div>
+                <h2>
+                    Customers who bought also...
+                </h2>
+                <h5>
+                    Page X of Y
+                </h5>
+                <button>Left</button>
+                <div>
+                    This is where the other items will go
+                </div>
+                <button>Right</button>
+            </div>
+        )
+    }
+}
+
+export default CWBA;
