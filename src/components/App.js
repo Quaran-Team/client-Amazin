f1ea219d7a6dac18f933d9662a8a2b39d9fb0ff9
Brandt Campbell <BCampbell@talentpath.com>
2020-07-22 10:31:04 -0400

    created Q&A component

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
+import QAcomponent from './QAcomponent';
 
 class App extends Component {
     render(){
         return(
             <div>
-                <h1>My React App</h1>
+                <QAcomponent />
             </div>
         )
     }
 }
 
 export default App;
+
