524a8e595a618155f761b9b6e5dc62faf2b5af27
Michal Terranova <MTerranova@talentpath.com>
2020-07-27 13:11:04 -0400

    browser router and state working

--- src/components/Directory.js
+++ src/components/Directory.js
@@ -2,12 +2,12 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import App from './App.jsx';
 import React, { Component } from 'react';
 import FrontPage from './FrontPage'
-import Product from './productVariant/Product.jsx'
-import CustomerReviewsContainer from './CustomerReviewsContainer';
-import CWBA from './CustomerWhoBoughtAlsoCmpnt/CustomersWhoBoughtAlsoAll'
-import QAcomponent from './QAcomponent';
-import ComparisonGrid from './ComparisonGrid';
-import CImagesAll from './CustomerImageCmpnt/CustomerImagesAll';
+// import Product from './productVariant/Product.jsx'
+// import CustomerReviewsContainer from './CustomerReviewsContainer';
+// import CWBA from './CustomerWhoBoughtAlsoCmpnt/CustomersWhoBoughtAlsoAll'
+// import QAcomponent from './QAcomponent';
+// import ComparisonGrid from './ComparisonGrid';
+// import CImagesAll from './CustomerImageCmpnt/CustomerImagesAll';
 
 class Directory extends Component {
     render(){
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Directory from './components/Directory.js'
+import Directory from './components/Directory.js';
 
 ReactDOM.render(<Directory />, document.getElementById("app"))
