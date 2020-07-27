b7d1989b340a7de9cdb63da35c336e71cf7b1a7a
Michal Terranova <MTerranova@talentpath.com>
2020-07-27 07:34:18 -0400

    skeleton

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -10,7 +10,7 @@ class App extends Component {
     render(){
         return(
             <div>
-                <h1>My React App</h1>
+                {/* <h1>My React App</h1> */}
                 <Product />
                 {/* <CustomerReviewsContainer />
                 <QAcomponent />
