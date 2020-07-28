d26dd19c6f667db7af2590a83cca0f9ceed1a896
Brandt Campbell <BCampbell@talentpath.com>
2020-07-28 10:43:52 -0400

    answers and single answer component plus QAndADataService created

--- src/components/QAitem.js
+++ src/components/QAitem.js
@@ -1,11 +1,36 @@
 import React, { Component } from 'react'
+import Answers from './Answers'
+import QAndADataService from '../service/QAndADataService'
+
 
 export default class QAitem extends Component {
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            response:null,
+        }
+        this.refreshQAndA = this.refreshQAndA.bind(this)
+    }
+
+    componentDidMount() {
+        this.refreshQAndA();
+    }
+
+    refreshQAndA() {
+        QAndADataService.retrieveQAndA(1)
+        .then(
+            response => {
+                this.setState({ response: response.data })
+            }
+        )
+    }
+
     render() {
         return (
             <div className="QAitem">
-                <h2>Question</h2>
-                <h2>Answer</h2>
+                <h2>Question: </h2>
+                <h2><Answers/></h2>
             </div>
         )
     }
