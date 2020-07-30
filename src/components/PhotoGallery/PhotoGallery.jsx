1c0c88a65710812421885f2e162c783ed83bba88
Trevis42 <sirtrevis42@gmail.com>
2020-07-30 14:26:07 -0500

    scrolling fuctionallity for thumbnail container

--- src/components/PhotoGallery/PhotoGallery.jsx
+++ src/components/PhotoGallery/PhotoGallery.jsx
@@ -7,10 +7,10 @@ export default class PhotoGallery extends Component {
 	render() {
 		return (
 			<div className="photo-gallery-container">
-				<div>
+				<div className="pg_thumbnails">
 					<PG_Thumbnails></PG_Thumbnails>
 				</div>
-				<div>
+				<div className="pg_expanded">
 					<PG_PhotoExpanded></PG_PhotoExpanded>
 				</div>
 			</div>
