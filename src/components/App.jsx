4e6e8c860e93c1e5e3899f7d38749c98add615ea
Trevis42 <sirtrevis42@gmail.com>
2020-07-30 10:59:50 -0500

    stubbed out images for testing placement

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,48 +4,49 @@ import BlueSecondTopLine from "./SecondTopLine/BlueSecondTopline";
 import Footer from "./Footer/Footer";
 import Axios from "axios";
 import Appples from "./RatingSummary/Appples";
+import PhotoGallery from "./PhotoGallery/PhotoGallery.jsx";
 
 class App extends Component {
-  state = {
-    items: [],
-  };
+	state = {
+		items: [],
+	};
 
-  componentDidMount() {
-    this.Items();
-  }
+	componentDidMount() {
+		this.Items();
+	}
 
-  Items = () => {
-    Axios({
-      method: "GET",
-      url: `http://localhost:8080/all/products/`,
-    }).then((res) => {
-      this.setState({ items: res.data.slice(1) });
-    });
-  };
+	Items = () => {
+		Axios({
+			method: "GET",
+			url: `http://localhost:8080/all/products/`,
+		}).then((res) => {
+			this.setState({ items: res.data.slice(1) });
+		});
+	};
 
-  render() {
-    return (
-      <div className="app">
-        <div>
-          <TopBar />
-        </div>
-        <BlueSecondTopLine />
-        <h1 style={{ textAlign: "center" }}>View our Products</h1>
-        <div className="item-div-container">
-          {this.state.items.map((item) => (
-            <a href={"/singleItem/" + item.itemId}>
-              <button id="frontpage-btn">
-                <h3>{item.itemName}</h3>
-                <p>{item.category}</p>
-              </button>
-            </a>
-          ))}
-        </div>
-        <Footer />
-        <Appples />
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className="app">
+				<div>
+					<TopBar />
+				</div>
+				<BlueSecondTopLine />
+				<h1 style={{ textAlign: "center" }}>View our Products</h1>
+				<div className="item-div-container">
+					{this.state.items.map((item) => (
+						<a href={"/singleItem/" + item.itemId}>
+							<button id="frontpage-btn">
+								<h3>{item.itemName}</h3>
+								<p>{item.category}</p>
+							</button>
+						</a>
+					))}
+				</div>
+				<Footer />
+				<Appples />
+			</div>
+		);
+	}
 }
 
 export default App;
