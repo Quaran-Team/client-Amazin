cb501d7a37a5a464b53df9afdf509238c3e7ed68
Trevis42 <sirtrevis42@gmail.com>
2020-07-30 10:36:00 -0500

    changed to .jsx files

--- src/components/PhotoGallery/PhotoGalleryThumbnail.jsx
+++ src/components/PhotoGallery/PhotoGalleryThumbnail.jsx
@@ -1,27 +1,8 @@
 import React, { Component } from "react";
-import "./PhotoThumbnail.css";
+
 export class PhotoGalleryThumbnail extends Component {
-	constructor(props) {
-		super(props);
-	}
 	render() {
-		const width = 40;
-		const height = 30;
-		const imgCount = 10;
-		var ids = [];
-		for (let i = 0; i < imgCount; i++) {
-			ids.push(i);
-		}
-		var imgs = ids.map((id, i) => {
-			return (
-				<img
-					onMouseEnter={() => this.props.updateHoverState(id)}
-					key={i}
-					src={`http://picsum.photos/id/${id.toString()}/${width.toString()}/${height.toString()}`}
-				/>
-			);
-		});
-		return <div className="photo-thumbnail">{imgs}</div>;
+		return <div></div>;
 	}
 }
 
