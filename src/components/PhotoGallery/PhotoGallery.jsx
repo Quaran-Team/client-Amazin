9e41a63ea42378220fea7bec7b8ac661ded64c6f
Trevis42 <sirtrevis42@gmail.com>
2020-07-30 14:00:10 -0500

    adding basic css and placeholder images

--- src/components/PhotoGallery/PhotoGallery.jsx
+++ src/components/PhotoGallery/PhotoGallery.jsx
@@ -1,14 +1,19 @@
 import React, { Component } from "react";
 import { default as PG_PhotoExpanded } from "./PhotoGalleryPhotoExpanded";
 import { default as PG_Thumbnails } from "./PhotoGalleryThumbnail";
+import "./PhotoGallery.css";
 
 export default class PhotoGallery extends Component {
 	render() {
 		return (
-			<React.Fragment>
-				<PG_Thumbnails></PG_Thumbnails>
-				<PG_PhotoExpanded></PG_PhotoExpanded>
-			</React.Fragment>
+			<div className="photo-gallery-container">
+				<div>
+					<PG_Thumbnails></PG_Thumbnails>
+				</div>
+				<div>
+					<PG_PhotoExpanded></PG_PhotoExpanded>
+				</div>
+			</div>
 		);
 	}
 }
