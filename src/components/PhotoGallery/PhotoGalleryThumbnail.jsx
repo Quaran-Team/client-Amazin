5ffe95d0299f9798a19e315148ad1f47974c0a1e
Trevis42 <sirtrevis42@gmail.com>
2020-07-30 14:26:07 -0500

    scrolling fuctionallity for thumbnail container

--- src/components/PhotoGallery/PhotoGalleryThumbnail.jsx
+++ src/components/PhotoGallery/PhotoGalleryThumbnail.jsx
@@ -3,13 +3,17 @@ import React, { Component } from "react";
 export class PhotoGalleryThumbnail extends Component {
 	render() {
 		return (
-			<React.Fragment className="photo-thumbnail">
+			<div className="photo-thumbnail">
 				<img src="https://picsum.photos/id/0/128/72" />
 				<img src="https://picsum.photos/id/1/128/72" />
 				<img src="https://picsum.photos/id/3/128/72" />
+				<img src="https://picsum.photos/id/4/128/72" />
+				<img src="https://picsum.photos/id/5/128/72" />
 				<img src="http://via.placeholder.com/128x72" />
 				<img src="http://via.placeholder.com/128x72" />
-			</React.Fragment>
+				<img src="http://via.placeholder.com/128x72" />
+				<img src="http://via.placeholder.com/128x72" />
+			</div>
 		);
 	}
 }
