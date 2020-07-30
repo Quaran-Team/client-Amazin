91e670d3e7ac811f65993cbf6756e5fcc39500ed
Trevis42 <sirtrevis42@gmail.com>
2020-07-30 10:59:50 -0500

    stubbed out images for testing placement

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,40 +1,20 @@
 import React, { Component } from "react";
 import Product from "./productVariant/Product.jsx";
-import CustomerReviewsContainer from "./CustomerReviews/CustomerReviews";
+import CustomerReviewsContainer from "./CustomerReviews/CustomerReviewsContainer";
 import CWBA from "./CustomerWhoBoughtAlsoCmpnt/CustomersWhoBoughtAlsoAll";
 import QAcomponent from "./QAcomponent";
 import ComparisonGrid from "./ComparisonGrid/ComparisonGrid.jsx";
 import CImagesAll from "./CustomerImageCmpnt/CustomerImagesAll";
 import Appples from "./RatingSummary/Appples";
-import PhotoGallery from "./PhotoGallery/PhotoGallery";
-import "../Css/App.css";
+import PhotoGallery from "./PhotoGallery/PhotoGallery.jsx";
 
 class App extends Component {
-	state = {
-		items: [],
-	};
-
-	componentDidMount() {
-		this.Items();
-	}
-
-	Items = () => {
-		Axios({
-			method: "GET",
-			url: `http://localhost:8080/all/products/`,
-		}).then((res) => {
-			this.setState({ items: res.data.slice(1) });
-		});
-	};
-
 	render() {
 		return (
-			<div className="app">
+			<div>
 				<h1>My React App</h1>
 				<PhotoGallery />
-				<div id="PurchaseItemContainer_Placeholder">
-					{/*place holder: replace when ready*/}
-				</div>
+				<Product />
 				<CustomerReviewsContainer />
 				<QAcomponent />
 				<ComparisonGrid />
