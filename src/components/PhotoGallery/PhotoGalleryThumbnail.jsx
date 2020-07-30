e015124b72ccb91a3f3dd2ce470d1ad89e40dd04
Trevis42 <sirtrevis42@gmail.com>
2020-07-30 10:59:50 -0500

    stubbed out images for testing placement

--- src/components/PhotoGallery/PhotoGalleryThumbnail.jsx
+++ src/components/PhotoGallery/PhotoGalleryThumbnail.jsx
@@ -2,7 +2,12 @@ import React, { Component } from "react";
 
 export class PhotoGalleryThumbnail extends Component {
 	render() {
-		return <div></div>;
+		return (
+			<div>
+				<img src="https://picsum.photos/id/0/90/60"></img>
+				<img src="https://picsum.photos/id/1/90/60"></img>
+			</div>
+		);
 	}
 }
 
