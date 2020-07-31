47361a6b52281f0f4c2f1b388b20a7c1cc340a54
Trevis42 <sirtrevis42@gmail.com>
2020-07-31 11:03:36 -0500

    fixed formating

--- src/components/CustomerReviews/CustomerReviews.jsx
+++ src/components/CustomerReviews/CustomerReviews.jsx
@@ -5,90 +5,51 @@ import ReviewMentions from "./ReviewMentions";
 import StarRating from "./StarRating";
 import Grid from "@material-ui/core/Grid";
 import CustomerReviewsDataService from "../../service/CustomerReviewsDataService";
-import GoodChartMock from "../RatingSummary/goodChartmock";
-import Ratings from "../productVariant/variant-Components/Ratings";
 
 class CustomerReviews extends Component {
 	constructor(props) {
 		super(props);
+		this.state = {
+			response: null,
+		};
 		this.refreshCustomerReviews = this.refreshCustomerReviews.bind(this);
 	}
-
-	state = {
-		review: [],
-	};
 
 	componentDidMount() {
 		this.refreshCustomerReviews();
 	}
 
 	refreshCustomerReviews() {
-		CustomerReviewsDataService.retrieveAllCustomerReviews().then(
+		CustomerReviewsDataService.retrieveCustomerReviews(1).then(
 			(response) => {
-				this.setState({ review: response.data });
+				this.setState({ response: response.data });
 			}
 		);
 	}
-
-	mapping = () => {
-		console.log(this.state.review);
-		return (
-			<div>
-				{this.state.review.map((resp) => (
-					<div>
-						<ReviewMentions
-							id={resp.id}
-							rate={resp.rating}
-							body={resp.reviewBody}
-							title={resp.reviewTitle}
-							tag={resp.reviewTag}
-							name={resp.reviewer}
-						/>
-					</div>
-				))}
-			</div>
-		);
-	};
 
 	render() {
 		return (
 			<div>
 				<h1> Customer Reviews</h1>
-
-				<Grid
-					container
-					justify="flex-start"
-					alignItems="flex-start"
-					id="left-grid"
-				>
+				<Grid container justify="flex-start" alignItems="flex-start">
 					<Grid item sm={4}>
 						<div>
-							<GoodChartMock />
+							<StarRating />
 						</div>
-
-						<div id="review-feature">
-							{" "}
-							<hr />{" "}
-							<h3>
-								<strong>By feature</strong>
-							</h3>
-							<Ratings id={this.props.params} />
-						</div>
-						<a>See more</a>
-						<hr />
-
-						<div className="review-product">
+						<br />
+						<div>
 							<ReviewProduct />
 						</div>
 					</Grid>
 
-					<Grid item sm id="right-grid">
+					<Grid item sm>
 						<div>
 							<CustomerImages />
 						</div>
 						<br />
-						{this.mapping()}
-						{/* {this.state.review.map( <div><ReviewMentions id={resp.id} rate={resp.rating} body={resp.reviewBody} title={resp.reviewTitle} tag={resp.reviewTag} name={resp.reviewer}/></div>)} */}
+						<div>
+							<ReviewMentions />
+						</div>
 					</Grid>
 				</Grid>
 			</div>
