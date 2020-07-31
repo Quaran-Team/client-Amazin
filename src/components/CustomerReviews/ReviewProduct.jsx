c37cf0f111396541327d4c56e89ecf175b16c58e
Bilikis Orulebaja <borulebaja@talentpath.com>
2020-07-31 10:19:16 -0500

    added some styling

--- src/components/CustomerReviews/ReviewProduct.jsx
+++ src/components/CustomerReviews/ReviewProduct.jsx
@@ -1,13 +1,26 @@
 import React, { Component } from 'react';
 
 class ReviewProduct extends Component {
+    constructor(){
+        super()
+        this.state = {
+
+        }
+
+        this.handleReview = this.handleReview.bind(this);
+    }
+
+    handleReview(e) {
+        alert("My product review!")
+        e.preventDefault(e);
+    }
     
     render() { 
         return ( 
             <div>
                 <h3>Review this product</h3>
                 <h5>Share your thoughts with other customers</h5>
-                <a href="link to review form"><button>Write a customer review</button></a>
+                <a href="link to review form"><button onClick={this.handleReview}>Write a customer review</button></a>
             </div>
          );
     }
