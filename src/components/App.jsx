b1524b6ff6bb02abd16d5a4ab3308cef2749266b
Trevis42 <sirtrevis42@gmail.com>
2020-07-31 10:34:38 -0500

    comment about initial css setup

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,7 +7,7 @@ import ComparisonGrid from "./ComparisonGrid/ComparisonGrid.jsx";
 import CImagesAll from "./CustomerImageCmpnt/CustomerImagesAll";
 import Appples from "./RatingSummary/Appples";
 import PhotoGallery from "./PhotoGallery/PhotoGallery.jsx";
-import "../Css/App.css";
+import "../Css/App.css"; //This is set to color every other member in App. Helps track placement.
 
 class App extends Component {
 	render() {
