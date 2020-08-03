c45eaa10d13af56162fc80c8782a0c8f54f0497d
Trevis42 <sirtrevis42@gmail.com>
2020-08-03 08:54:23 -0500

    moded css around; added hover css.

--- src/components/PhotoGallery/PhotoGalleryThumbnail.jsx
+++ src/components/PhotoGallery/PhotoGalleryThumbnail.jsx
@@ -1,7 +1,9 @@
 import React, { Component } from "react";
-
+import "./PhotoThumbnail.css";
 export class PhotoGalleryThumbnail extends Component {
 	render() {
+		var num = 128;
+		var num2 = 72;
 		return (
 			<div className="photo-thumbnail">
 				<img src="https://picsum.photos/id/0/128/72" />
@@ -11,8 +13,12 @@ export class PhotoGalleryThumbnail extends Component {
 				<img src="https://picsum.photos/id/5/128/72" />
 				<img src="http://via.placeholder.com/128x72" />
 				<img src="http://via.placeholder.com/128x72" />
-				<img src="http://via.placeholder.com/128x72" />
-				<img src="http://via.placeholder.com/128x72" />
+				<img
+					src={
+						"http://picsum.photos/id/6/" +
+						`${num.toString()}/${num2.toString()}`
+					}
+				/>
 			</div>
 		);
 	}
