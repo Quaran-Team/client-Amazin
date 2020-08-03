7aefdeb09e3ae687af4ee0c7f282c4cf07b4f679
Trevis42 <sirtrevis42@gmail.com>
2020-08-03 13:52:38 -0500

    added variables for sizing of images

--- src/components/PhotoGallery/PhotoGalleryPhotoExpanded.jsx
+++ src/components/PhotoGallery/PhotoGalleryPhotoExpanded.jsx
@@ -5,12 +5,17 @@ export class PhotoGalleryPhotoExpanded extends Component {
 	// 	super(props);
 	// }
 	render() {
-		const width = 640;
-		const height = 320;
+		const i = 0;
+		var width = 640;
+		var height = 320;
 		return (
 			<React.Fragment className="photo-expanded">
 				<img
-					src={`http://picsum.photos/id/${this.props.imgId.toString()}/${width.toString()}/${height.toString()}`}
+					src={
+						"http://picsum.photos/id/" +
+						`${i.toString()}/` +
+						`${width.toString()}/${height.toString()}`
+					}
 				></img>
 			</React.Fragment>
 		);
