5de40182595aee42ddbaf1e3f551a0da57f8be15
Trevis42 <sirtrevis42@gmail.com>
2020-08-03 08:54:23 -0500

    moved css around; added hover css.

--- src/components/PhotoGallery/PhotoGalleryThumbnail.jsx
+++ src/components/PhotoGallery/PhotoGalleryThumbnail.jsx
@@ -5,23 +5,25 @@ export class PhotoGalleryThumbnail extends Component {
 		super(props);
 	}
 	render() {
-		const width = 40;
-		const height = 30;
-		const imgCount = 10;
-		var ids = [];
-		for (let i = 0; i < imgCount; i++) {
-			ids.push(i);
-		}
-		var imgs = ids.map((id, i) => {
-			return (
+		var num = 128;
+		var num2 = 72;
+		return (
+			<div className="photo-thumbnail">
+				<img src="https://picsum.photos/id/0/128/72" />
+				<img src="https://picsum.photos/id/1/128/72" />
+				<img src="https://picsum.photos/id/3/128/72" />
+				<img src="https://picsum.photos/id/4/128/72" />
+				<img src="https://picsum.photos/id/5/128/72" />
+				<img src="http://via.placeholder.com/128x72" />
+				<img src="http://via.placeholder.com/128x72" />
 				<img
-					onMouseEnter={() => this.props.updateHoverState(id)}
-					key={i}
-					src={`http://picsum.photos/id/${id.toString()}/${width.toString()}/${height.toString()}`}
+					src={
+						"http://picsum.photos/id/6/" +
+						`${num.toString()}/${num2.toString()}`
+					}
 				/>
-			);
-		});
-		return <div className="photo-thumbnail">{imgs}</div>;
+			</div>
+		);
 	}
 }
 
