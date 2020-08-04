9366cc1310180a293bb58e60a9de2fc66c548649
Trevis42 <sirtrevis42@gmail.com>
2020-08-04 14:19:20 -0500

    fix mixxing return statement

--- src/components/productVariant/variant-Components/Availability.jsx
+++ src/components/productVariant/variant-Components/Availability.jsx
@@ -138,10 +138,12 @@ class Availability extends Component {
 
 				//this is if the stock supply is empty
 			);
-		} else if (inStock === 0) {
-			<div className="out-of-stock-banner">
-				<h3>Sorry, this item is out of Stock!</h3>
-			</div>;
+		} else if (parseInt(inStock) === 0) {
+			return (
+				<div className="out-of-stock-banner">
+					<h3>Sorry, this item is out of Stock!</h3>
+				</div>
+			);
 
 			// this is the default scenario
 		} else {
