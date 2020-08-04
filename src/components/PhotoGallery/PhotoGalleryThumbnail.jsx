9d0ff7a40b8246320a45ddda7ddc82ea9c8e2853
Trevis42 <sirtrevis42@gmail.com>
2020-08-04 11:26:55 -0500

    thumbnail hovers chagnes expanded photo

--- src/components/PhotoGallery/PhotoGalleryThumbnail.jsx
+++ src/components/PhotoGallery/PhotoGalleryThumbnail.jsx
@@ -1,25 +1,31 @@
 import React, { Component } from "react";
 import "./PhotoThumbnail.css";
 export class PhotoGalleryThumbnail extends Component {
+	constructor(props) {
+		super(props);
+	}
 	render() {
-		var width = 40;
-		var height = 30;
+		const width = 40;
+		const height = 30;
 		const imgCount = 9;
 		var ids = [];
 		for (let i = 0; i < imgCount; i++) {
 			ids.push(i);
 		}
-
-		var imgs = ids.map((id, i) => (
-			<img
-				key={i}
-				src={
-					"http://picsum.photos/id/" +
-					`${i.toString()}/` +
-					`${width.toString()}/${height.toString()}`
-				}
-			/>
-		));
+		var imgs = ids.map((id, i) => {
+			console.log(id);
+			return (
+				<img
+					onMouseEnter={() => this.props.updateHoverState(id)}
+					key={i}
+					src={
+						"http://picsum.photos/id/" +
+						`${id.toString()}/` +
+						`${width.toString()}/${height.toString()}`
+					}
+				/>
+			);
+		});
 		return <div className="photo-thumbnail">{imgs}</div>;
 	}
 }
