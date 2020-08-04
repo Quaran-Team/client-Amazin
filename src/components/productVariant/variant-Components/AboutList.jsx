0d7bd09f440b0f21de8e7f77396cf85728d4cc4a
Michal Terranova <michalterranova@mail.com>
2020-08-04 11:22:25 -0400

    Update AboutList.jsx
    fixed header

--- src/components/productVariant/variant-Components/AboutList.jsx
+++ src/components/productVariant/variant-Components/AboutList.jsx
@@ -1,10 +1,6 @@
 import React from 'react'; 
 
 function AboutList(props) {
-<<<<<<< HEAD
-=======
-    console.log(props)
->>>>>>> 0d2c2b9f41c9a8da4145bafb7c1b957cd66ac2c1
     //this is my work around so there doesn't need to be another table. I used * to separate everything. 
     //Feel free to use any strange character you want though, but you have to change the seeding in the database.
     let splittingItems = props.about_item.split("*"); 
@@ -14,11 +10,7 @@ function AboutList(props) {
     return(
         <div>
                 <ul>
-<<<<<<< HEAD
                     {splittingItems.map(point => <li key={point}>{point}</li>)}
-=======
-                    {splittingItems.map(point => <li>{point}</li>)}
->>>>>>> 0d2c2b9f41c9a8da4145bafb7c1b957cd66ac2c1
                 </ul>
         </div>
     )
