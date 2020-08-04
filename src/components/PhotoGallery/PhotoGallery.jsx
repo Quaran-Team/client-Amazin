ee5a03a9a3c4278d1d8261fd9a754820828e56c3
Trevis42 <sirtrevis42@gmail.com>
2020-08-04 11:26:55 -0500

    thumbnail hovers chagnes expanded photo

--- src/components/PhotoGallery/PhotoGallery.jsx
+++ src/components/PhotoGallery/PhotoGallery.jsx
@@ -4,14 +4,27 @@ import { default as PG_Thumbnails } from "./PhotoGalleryThumbnail";
 import "../../Css/PhotoGallery.css";
 
 export default class PhotoGallery extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			id: 0,
+		};
+	}
+
+	updateHoverState = (i) => {
+		this.setState({ id: i });
+		console.log(`imageId: ${i}`);
+	};
 	render() {
 		return (
 			<div className="photo-gallery-container">
 				<div className="pg_thumbnails">
-					<PG_Thumbnails></PG_Thumbnails>
+					<PG_Thumbnails
+						updateHoverState={this.updateHoverState}
+					></PG_Thumbnails>
 				</div>
 				<div className="pg_expanded">
-					<PG_PhotoExpanded></PG_PhotoExpanded>
+					<PG_PhotoExpanded imgId={this.state.id}></PG_PhotoExpanded>
 				</div>
 			</div>
 		);
