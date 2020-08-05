cd0c0aed08f9d7cf5d872c0fae8c370e39415d3c
Trevor Taylor <sirtrevis42@gmail.com>
2020-08-05 13:53:45 -0500

    Feature/photogallery closes #73 (#85)
    * update gitignore to exclude yarn.lock
    
    * added basic photo gallery components
    
    * changed to .jsx files
    
    * stubbed out images for testing placement
    
    * adding basic css and placeholder images
    
    * scrolling fuctionallity for thumbnail container
    
    * scrollable thumbnail with hidden scrollbar
    
    * comment about initial css setup
    
    * fixed formating
    
    * commented out Products component in app
    
    * moded css around; added hover css.
    
    * added variables for sizing of images
    
    * updated css
    
    * thumbnail hovers chagnes expanded photo
    
    * removed console log
    
    * removed product from app.jsx
    
    * fix mixxing return statement
    
    * commented out unneeded break
    
    * formated directory
    
    * added import for Photogallery component
    
    * added app css
    
    * change img url to string interpolation
    
    * change img url to string interpolation

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,25 +1,30 @@
-import React, { Component } from 'react';
-import CustomerReviewsContainer from './CustomerReviews/CustomerReviews';
-import CWBA from './CustomerWhoBoughtAlsoCmpnt/CustomersWhoBoughtAlsoAll'
-import QAcomponent from './QAcomponent';
-import ComparisonGrid from './ComparisonGrid/ComparisonGrid.jsx';
-import CImagesAll from './CustomerImageCmpnt/CustomerImagesAll';
-import Appples from './RatingSummary/Appples';
+import React, { Component } from "react";
+import CustomerReviewsContainer from "./CustomerReviews/CustomerReviews";
+import CWBA from "./CustomerWhoBoughtAlsoCmpnt/CustomersWhoBoughtAlsoAll";
+import QAcomponent from "./QAcomponent";
+import ComparisonGrid from "./ComparisonGrid/ComparisonGrid.jsx";
+import CImagesAll from "./CustomerImageCmpnt/CustomerImagesAll";
+import Appples from "./RatingSummary/Appples";
+import PhotoGallery from "./PhotoGallery/PhotoGallery";
+import "../Css/App.css";
 
 class App extends Component {
-    render(){
-        return(
-            <div>
-                <CustomerReviewsContainer />
-                <QAcomponent />
-                <ComparisonGrid/>
-                <CImagesAll />
-                <CWBA/>
-                <Appples/>
-            </div>
-        )
-    }
+	render() {
+		return (
+			<div className="app">
+				<PhotoGallery />
+				<div id="PurchaseItemContainer_Placeholder">
+					{/*place holder: replace when ready*/}
+				</div>
+				<CustomerReviewsContainer />
+				<QAcomponent />
+				<ComparisonGrid />
+				<CImagesAll />
+				<CWBA />
+				<Appples />
+			</div>
+		);
+	}
 }
 
 export default App;
-
---
+++ src/components/PhotoGallery/PhotoGalleryPhotoExpanded.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+
+export class PhotoGalleryPhotoExpanded extends Component {
+	// constructor(props) {
+	// 	super(props);
+	// }
+	render() {
+		const width = 640;
+		const height = 320;
+		return (
+			<React.Fragment className="photo-expanded">
+				<img
+					src={`http://picsum.photos/id/${this.props.imgId.toString()}/${width.toString()}/${height.toString()}`}
+				></img>
+			</React.Fragment>
+		);
+	}
+}
+
+export default PhotoGalleryPhotoExpanded;
