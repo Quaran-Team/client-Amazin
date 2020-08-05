e6bb1e1c46abedb3e1239458a66cd855e92f7d97
Trevis42 <sirtrevis42@gmail.com>
2020-08-05 07:42:55 -0500

    formated directory

--- src/components/Directory.js
+++ src/components/Directory.js
@@ -2,7 +2,6 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import App from "./App.jsx";
 import React, { Component } from "react";
 import PrimaryPage from "./PrimaryPage";
-import UnderConstruction from "./ExtraPages.jsx/UnderConstruction.jsx";
 
 class Directory extends Component {
 	render() {
@@ -11,8 +10,6 @@ class Directory extends Component {
 				<Switch>
 					<Route exact path="/" component={App} />
 					<Route path="/singleItem/:id" component={PrimaryPage} />
-					<Route path="/ninja_stuff" component={UnderConstruction} />
-					<Route path="/microwave_stuff" component={UnderConstruction}/>
 				</Switch>
 			</Router>
 		);
