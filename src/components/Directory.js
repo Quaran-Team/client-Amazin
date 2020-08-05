0579db05300d1bf84ab8940b232e063b2b1c816a
Trevis42 <sirtrevis42@gmail.com>
2020-08-05 07:42:55 -0500

    formated directory

--- src/components/Directory.js
+++ src/components/Directory.js
@@ -1,22 +1,19 @@
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom"; 
-import App from './App.jsx';
-import React, { Component } from 'react';
-import PrimaryPage from './PrimaryPage';
-
-
-
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import App from "./App.jsx";
+import React, { Component } from "react";
+import PrimaryPage from "./PrimaryPage";
 
 class Directory extends Component {
-    render(){
-        return(
-        <Router>
-            <Switch>
-                <Route exact path="/" component={App} />
-                <Route path="/singleItem/:id" component={PrimaryPage} />
-            </Switch>
-        </Router>
-        )
-    }
+	render() {
+		return (
+			<Router>
+				<Switch>
+					<Route exact path="/" component={App} />
+					<Route path="/singleItem/:id" component={PrimaryPage} />
+				</Switch>
+			</Router>
+		);
+	}
 }
 
 export default Directory;
