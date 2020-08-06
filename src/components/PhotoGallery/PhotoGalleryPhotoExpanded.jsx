cf3259c474ae40528baec36ad495a0530b258e37
Trevis42 <sirtrevis42@gmail.com>
2020-08-06 13:58:40 -0500

    moved css and setup Zoom component

--- src/components/PhotoGallery/PhotoGalleryPhotoExpanded.jsx
+++ src/components/PhotoGallery/PhotoGalleryPhotoExpanded.jsx
@@ -10,11 +10,11 @@ export class PhotoGalleryPhotoExpanded extends Component {
 		const width = 640;
 		const height = 320;
 		return (
-			<React.Fragment className="photo-expanded">
+			<div className="photo-expanded">
 				<img
 					src={`http://picsum.photos/id/${this.props.imgId.toString()}/${width.toString()}/${height.toString()}`}
 				></img>
-			</React.Fragment>
+			</div>
 		);
 	}
 }
