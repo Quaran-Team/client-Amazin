ce7f1fc0fb0cb23864b262a794d16cfeea498319
Trevis42 <sirtrevis42@gmail.com>
2020-08-06 13:58:40 -0500

    moved css and setup Zoom component

---
+++ src/components/PhotoGallery/PhotoGalleryPhotoZoom.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+export class PhotoGalleryPhotoZoom extends Component {
+	render() {
+		return <div className="photo-zoom-container"></div>;
+	}
+}
+
+export default PhotoGalleryPhotoZoom;
