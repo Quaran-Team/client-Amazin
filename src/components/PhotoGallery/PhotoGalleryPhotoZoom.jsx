39d6476ed4e11b2298fce7286370bd3f9f65df62
Trevis42 <sirtrevis42@gmail.com>
2020-08-07 11:05:03 -0500

    removed unused component

--- src/components/PhotoGallery/PhotoGalleryPhotoZoom.jsx
+++
@@ -1,9 +0,0 @@
-import React, { Component } from "react";
-
-export class PhotoGalleryPhotoZoom extends Component {
-	render() {
-		return <div className="photo-zoom-container"></div>;
-	}
-}
-
-export default PhotoGalleryPhotoZoom;
