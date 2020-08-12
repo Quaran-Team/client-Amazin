cd73f91e782c923579879f1097abd5be03494fc3
BLoveless <bloveless@talentpath.com>
2020-08-12 17:20:21 -0400

    lil bug in top

--- src/components/TopBar/TopBar.js
+++ src/components/TopBar/TopBar.js
@@ -8,14 +8,13 @@ import Prime from "./Prime";
 import Cart from "./Cart";
 import WorkinMenu from "./WorkinMenu";
 import './topbar.css';
-import WorkinMenu from "./WorkinMenu";
+
 
 export default class TopBar extends Component {
   render() {
     return (
       <div className="topbar">
         <WorkinMenu/>
-        {/* <Menu/> */}
         <Logo/>
         <SearchBar/>
         <Flag/>
