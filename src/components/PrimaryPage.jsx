9b5e81f8f8b6c28dda34a290060611182ba6b1d1
Michal Terranova <MTerranova@talentpath.com>
2020-08-12 17:02:42 -0400

    conflict resolved

--- src/components/PrimaryPage.jsx
+++ src/components/PrimaryPage.jsx
@@ -7,13 +7,8 @@ import ComparisonGrid from "./ComparisonGrid/ComparisonGrid.jsx";
 import CImagesAll from "./CustomerImageCmpnt/CustomerImagesAll";
 import Appples from "./RatingSummary/Appples";
 import Grid from "@material-ui/core/Grid";
-<<<<<<< HEAD
-import './primarypage.css'
-import Manufacturer from './Manufacturer/Manufacturer'
-=======
 import TopBar from "./TopBar/TopBar";
 import "./primarypage.css";
->>>>>>> 458e38bd4b420b3f7070fbe28979c759de72a88c
 
 class PrimaryPage extends Component {
 
