2068e4e1024c40ae7501992c7248cfa6af700b75
Brandt Campbell <BCampbell@talentpath.com>
2020-08-13 13:30:17 -0400

    votes section created for individuality

--- src/components/QAitem.js
+++ src/components/QAitem.js
@@ -1,13 +1,14 @@
 import React, { Component } from "react";
 import QAndADataService from "../service/QAndADataService";
+import Votes from "./Votes"
 import "./qacomponent.css";
 
 export default class QAitem extends Component {
   constructor(props) {
     super(props);
     // this.handleChange = this.handleChange.bind(this);
-    this.handleChangeAdd = this.handleChangeAdd.bind(this);
-    this.handleChangeSubtract = this.handleChangeSubtract.bind(this);
+    // this.handleChangeAdd = this.handleChangeAdd.bind(this);
+    // this.handleChangeSubtract = this.handleChangeSubtract.bind(this);
 
   }
 
@@ -16,7 +17,7 @@ export default class QAitem extends Component {
     allAnswers: [],
     sortedAnswers: [],
     itemId: null,
-    input: 0,
+    // input: 0,
   };
 
   refreshQAndA = this.refreshQAndA.bind(this);
@@ -27,15 +28,15 @@ export default class QAitem extends Component {
     this.refreshAnswers();
   }
 
-  handleChangeAdd(e) {
-    let votes = parseInt(e)
-    this.setState({ input: votes + 1 });
-  }
+//   handleChangeAdd(e) {
+//     let votes = parseInt(e)
+//     this.setState({ input: votes + 1 });
+//   }
 
-  handleChangeSubtract(e) {
-    let votes = parseInt(e)
-    this.setState({ input: votes - 1 });
-  }
+//   handleChangeSubtract(e) {
+//     let votes = parseInt(e)
+//     this.setState({ input: votes - 1 });
+//   }
 
   refreshQAndA() {
     QAndADataService.retrieveAllQAndAs().then((response) => {
@@ -49,21 +50,6 @@ export default class QAitem extends Component {
     });
   }
 
-  addition = () => {
-    // let input = document.querySelector('input')
-    // let value = parseInt(this.state.input) + 1;
-    // this.setState({ input: value });
-    // console.log(input)
-    // input.value = parseInt(input.value) + 1;
-  };
-
-  subtraction = () => {
-    // let value = parseInt(this.state.input) - 1;
-    // this.setState({ input: value });
-    // let input = document.querySelector('input')
-    // input.value = parseInt(input.value) - 1;
-  };
-
   sortQuestions = (id) => {
     const filterQuestionsArray = this.state.response.filter(
       (question) => question.itemid == id
@@ -72,22 +58,7 @@ export default class QAitem extends Component {
       <div>
         {filterQuestionsArray.map((q) => (
           <div className="section">
-            <div className="votes">
-              <button id="add" onClick={ () => this.handleChangeAdd(this.state.input)}>
-                <i className="arrow up"></i>
-              </button>
-              <input
-                id="votes"
-                type="number"
-                value={this.state.input}
-                // onChange={(e) => {
-                //   this.handleChangeAdd(e);
-                // }}
-              />
-              <button id="subtract" onClick={ () => this.handleChangeSubtract(this.state.input)}>
-                <i className="arrow down"></i>
-              </button>
-            </div>
+            <Votes/>
             <div className="qAndA">
               <div className="question">
                 <h4>Question: </h4>
---
+++ src/components/Votes.js
@@ -0,0 +1,48 @@
+import React, { Component } from "react";
+import "./qacomponent.css";
+
+export default class Votes extends Component {
+  constructor(props) {
+    super(props);
+    this.handleChangeAdd = this.handleChangeAdd.bind(this);
+    this.handleChangeSubtract = this.handleChangeSubtract.bind(this);
+  }
+
+  state = {
+    votes: 0,
+  };
+
+  handleChangeAdd(e) {
+    let currentVotes = parseInt(e);
+    this.setState({ votes: currentVotes + 1 });
+  }
+
+  handleChangeSubtract(e) {
+    let currentVotes = parseInt(e);
+    this.setState({ votes: currentVotes - 1 });
+  }
+
+  render() {
+    return (
+      <div className="votes">
+        <button id="add" onClick={() => this.handleChangeAdd(this.state.votes)}>
+          <i className="arrow up"></i>
+        </button>
+        <input
+          id="votes"
+          type="number"
+          value={this.state.votes}
+          // onChange={(e) => {
+          //   this.handleChangeAdd(e);
+          // }}
+        />
+        <button
+          id="subtract"
+          onClick={() => this.handleChangeSubtract(this.state.votes)}
+        >
+          <i className="arrow down"></i>
+        </button>
+      </div>
+    );
+  }
+}
