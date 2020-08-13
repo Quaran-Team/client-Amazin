49a2b0e544095b9f15a26608ae8984d7e5f8257d
BLoveless <bloveless@talentpath.com>
2020-08-13 15:03:38 -0400

    it is unbroken

---
+++ src/components/Footer/Prefoot.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+// will move to footer
+
+export default class Prefoot extends Component {
+  render() {
+    return (
+      <div className="prefoot">
+        <img
+          className="lowfootertable"
+          src="/images/preFoot.jpg"
+          alt="a table of Amazin features"
+        />
+      </div>
+    );
+  }
+}
---
+++ src/components/SecondTopLine/Gift.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+export default class Gift extends Component {
+  render() {
+    return (
+      <div className="giftfind">
+        <h4>
+          <a className="bluetoprow">Find a Gift</a>
+        </h4>
+      </div>
+    );
+  }
+}
