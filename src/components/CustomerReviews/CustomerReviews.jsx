64129e6676495c2d9171e39019acd7f46fdc0514
Michal Terranova <michalterranova@mail.com>
2020-08-14 09:32:06 -0400

    Enhancement/erased cssback (#138)
    * package-lock
    
    * fixing some erased css
    
    * searching for work that got deleted in a merge somewhere
    
    * fixed header component
    
    * replaced files
    
    * header tag
    
    Co-authored-by: Michal Terranova <MTerranova@talentpath.com>

--- src/components/CustomerReviews/CustomerReviews.jsx
+++ src/components/CustomerReviews/CustomerReviews.jsx
@@ -9,190 +9,170 @@ import GoodChartMock from "../RatingSummary/goodChartmock";
 import Ratings from "../productVariant/variant-Components/Ratings";
 
 class CustomerReviews extends Component {
-  constructor(props) {
-    super(props);
-    this.refreshCustomerReviews = this.refreshCustomerReviews.bind(this);
-  }
+	constructor(props) {
+		super(props);
+		this.refreshCustomerReviews = this.refreshCustomerReviews.bind(this);
+	}
 
-  state = {
-    review: [],
-    dropmenu: "drop",
-    menuitem: "top",
-  };
+	state = {
+		review: [],
+		dropmenu: "drop",
+		menuitem: "top",
+		tags: []
+	};
 
-  componentDidMount() {
-    this.refreshCustomerReviews();
-  }
+	componentDidMount() {
+		this.refreshCustomerReviews();
+	}
 
-  refreshCustomerReviews() {
-    CustomerReviewsDataService.retrieveAllCustomerReviews().then((response) => {
-      this.setState({ review: response.data });
-      // const filteredArray = response.data.filter( item => item.itemId == this.props.params)
-      //this.setState({ review: filteredArray})
-    });
-  }
+    refreshCustomerReviews() {
+        CustomerReviewsDataService.retrieveAllCustomerReviews()
+        .then(
+            response => {
+				console.log("REVIEWWW")
+				console.log(response.data)
+                this.setState({ review: response.data })
+                // const filteredArray = response.data.filter( item => item.itemId == this.props.params)
+                //this.setState({ review: filteredArray})
+		}
+		)
+	}
+	menu = () => {
+		if (this.state.menuitem == "top") {
+			return (
+				<div>
+					<button id="top" onClick={() => this.top()}>
+						Top Reviews <i class="arrow down"></i>
+					</button>
+					<button id="most" onClick={() => this.most()}>
+						Most Recent{" "}
+					</button>
+				</div>
+			);
+		} else if (this.state.menuitem == "most") {
+			return (
+				<div>
+					<button id="most" onClick={() => this.most()}>
+						Most Recent <i class="arrow down"></i>
+					</button>
+					<button id="top" onClick={() => this.top()}>
+						Top Reviews{" "}
+					</button>
+				</div>
+			)
+		}
+	}
 
-  menu = () => {
-    if (this.state.menuitem == "top") {
-      return (
-        <div>
-          <button id="top" onClick={() => this.top()}>
-            Top Reviews <i className="arrow down"></i>
-          </button>
-          <button id="most" onClick={() => this.most()}>
-            Most Recent{" "}
-          </button>
-        </div>
-      );
-    } else if (this.state.menuitem == "most") {
-      return (
-        <div>
-          <button id="most" onClick={() => this.most()}>
-            Most Recent <i className="arrow down"></i>
-          </button>
-          <button id="top" onClick={() => this.top()}>
-            Top Reviews{" "}
-          </button>
-        </div>
-      );
-    }
-  };
-  top = () => {
-    this.setState({ menuitem: "top" });
-    this.menu(this.state.menuitem);
-  };
 
-  most = () => {
-    this.setState({ menuitem: "most" });
-    this.menu(this.state.menuitem);
-  };
-
-  mapping = () => {
-    console.log(this.state.review);
-    return (
-      <div>
-        {this.state.review.map((resp) => (
-          <div>
-            <ReviewMentions
-              id={resp.id}
-              rate={resp.rating}
-              body={resp.reviewBody}
-              title={resp.reviewTitle}
-              tag={resp.reviewTag}
-              name={resp.reviewer}
-            />
-          </div>
-        ))}
-      </div>
-    );
-  };
-
-  drop = () => {
-    if (this.state.dropmenu === "drop") {
-      this.setState({ dropmenu: "down" });
-    } else {
-      this.setState({ dropmenu: "drop" });
-    }
-  };
-
-  menu = () => {
-    if (this.state.menuitem == "top") {
-      return (
-        <div>
-          <button id="top" onClick={() => this.top()}>
-            Top Reviews <i className="arrow down"></i>
-          </button>
-          <button id="most" onClick={() => this.most()}>
-            Most Recent{" "}
-          </button>
-        </div>
-      );
-    } else if (this.state.menuitem == "most") {
-      return (
-        <div>
-          <button id="most" onClick={() => this.most()}>
-            Most Recent <i className="arrow down"></i>
-          </button>
-          <button id="top" onClick={() => this.top()}>
-            Top Reviews{" "}
-          </button>
-        </div>
-      );
-    }
-    top = () => {
-      this.setState({ menuitem: "top" });
-      this.menu(this.state.menuitem);
-    };
-    most = () => {
-      this.setState({ menuitem: "most" });
-      this.menu(this.state.menuitem);
-    };
-  };
-
-  render() {
-    return (
-      <div>
-        <h1> Customer Reviews</h1>
-        <Grid
-          container
-          justify="flex-start"
-          alignItems="flex-start"
-          id="left-grid"
-        >
-          <Grid item sm={4}>
+	top = () => {
+		this.setState({ menuitem: "top" });
+		this.menu(this.state.menuitem);
+	};
+	most = () => {
+		this.setState({ menuitem: "most" });
+		this.menu(this.state.menuitem);
+	};
+              
+    mapping = () => {
+        console.log(this.state.review)
+        return(
             <div>
-              <GoodChartMock />
+            {this.state.review.map( resp => <div><ReviewMentions id={resp.id} rate={resp.rating} body={resp.reviewBody} title={resp.reviewTitle} tag={resp.reviewTag} name={resp.reviewer}/></div>)}
             </div>
-            <div id="review-feature">
-              {" "}
-              <hr />{" "}
-              <h3>
-                <strong>By feature</strong>
-              </h3>
-              <Ratings id={this.props.params} />
-            </div>
-            <a>See more</a>
-            <hr />
-            <div className="review-product">
-              <ReviewProduct />
-            </div>
-          </Grid>
+        )
+    }
 
-          <Grid item sm id="right-grid">
+    drop = () => {
+		if(this.state.dropmenu === "drop") {
+			this.setState({dropmenu: 'down',
+		})
+		} else {
+			this.setState({ dropmenu: 'drop'
+		})
+		}
+    }
+    
+	menu = () => {
+		if(this.state.menuitem == 'top' ){
+			return (
+				<div><button id="top" onClick={() => this.top()}>Top Reviews <i class="arrow down"></i></button><button id="most" onClick={() => this.most()}>Most Recent   </button></div>
+			)
+		} else if(this.state.menuitem == 'most'){
+			return (
+				<div><button id="most" onClick={() => this.most()}>Most Recent <i class="arrow down"></i></button><button id="top" onClick={() => this.top()}>Top Reviews   </button></div>
+			)
+		}
+	}
+		top = () => {
+			this.setState({ menuitem: 'top'})
+			this.menu(this.state.menuitem)
+		}
+
+		most = () => {
+			this.setState({ menuitem: 'most'})
+			this.menu(this.state.menuitem)
+		}
+
+		btnMapping = () => {
+			let tag = [];
+			let finalAllTag = [];
+			//this collects tags from each of reviews and saves all to a final array.
+			this.state.review.map( btn => {
+				tag = btn.reviewTag.split(", ");
+				tag.map( sendFinal => finalAllTag.push(sendFinal))
+			})
+			console.log(finalAllTag)
+
+			//to ensure no duplicates
+			const distinct = ( value, index, self) => {
+				return self.indexOf(value) === index;
+			}
+			const distinctTags = finalAllTag.filter (distinct)
+
+			//finally return the button to the user with it's distinct tag
+			return( 
+				distinctTags.map( tag =>
+				<button id="button">{tag}</button>
+			))
+		}
+		
+    
+    render() { 
+        return ( 
             <div>
-              <CustomerImages />
+                <h1> Customer Reviews</h1>
+                
+                <Grid container justify="flex-start" alignItems="flex-start" id="left-grid">
+                    <Grid item sm={4}>
+                        <div><GoodChartMock/></div>
+                        
+                        <div id="review-feature"> <hr/> <h3><strong>By feature</strong></h3><Ratings id={this.props.params} /></div>
+                        <a>See more</a><hr/>
+                          
+                        <div className="review-product"><ReviewProduct /></div>
+                    </Grid>
+                    
+                    <Grid item sm id="right-grid">
+                        <div><CustomerImages /></div> 
+                        <br />
+                        <div className="review-mention">
+                    <h3>Read reviews that mention</h3>
+                    <span>{this.btnMapping()}</span>
+                </div>
+                    <br/>
+                    <br/>
+                   <div className="dropdown">
+                        <div id={this.state.dropmenu} onClick={()=> this.drop()}>
+					        {this.menu()}
+				        </div>
+                   </div>
+                   {this.state.review.map( resp => <div><ReviewMentions id={resp.id} rate={resp.rating} body={resp.reviewBody} title={resp.reviewTitle} tag={resp.reviewTag} name={resp.reviewer}/></div>)}
+                    </Grid>
+                </Grid>
+            
             </div>
-            <br />
-            <div className="review-mention">
-              <h3>Read reviews that mention</h3>
-              <span>
-                <button id="button">filtered button</button>
-              </span>
-            </div>
-            <br />
-            <br />
-            <div className="dropdown">
-              <div id={this.state.dropmenu} onClick={() => this.drop()}>
-                {this.menu()}
-              </div>
-            </div>
-            {this.state.review.map((resp) => (
-              <div>
-                <ReviewMentions
-                  id={resp.id}
-                  rate={resp.rating}
-                  body={resp.reviewBody}
-                  title={resp.reviewTitle}
-                  tag={resp.reviewTag}
-                  name={resp.reviewer}
-                />
-              </div>
-            ))}
-          </Grid>
-        </Grid>
-      </div>
-    );
-  }
+         );
+    }
 }
 
 export default CustomerReviews;
